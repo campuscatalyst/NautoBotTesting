8797e9b58d9e6577a6c8cdb42e8627519e0599bd
smadabat1 <mvspavan001@gmail.com>
2025-10-12 12:01:22 +0530

    Hello world update

--- jobs/hello_world.py
+++ jobs/hello_world.py
@@ -1,12 +1,12 @@
-from nautobot.apps import jobs
+from nautobot.apps.jobs import Job, register_jobs, StringVar
 
 name = "Examples"                 # Grouping shown in the UI
 
-class HelloWorldJob(jobs.Job):
+class HelloWorldJob(Job):
     class Meta:
         name = "Hello World"      # Job name
 
-    who = jobs.StringVar(         # One user‑supplied variable
+    who = StringVar(         # One user‑supplied variable
         description="Whom should we greet?",
         default="world",
     )
@@ -14,4 +14,4 @@ class HelloWorldJob(jobs.Job):
     def run(self, *, who):
         self.logger.info("Hello, %s!", who)  # This is the logic that runs when the Job is executed
 
-jobs.register_jobs(HelloWorldJob) # <- required in Nautobot 2.x
+register_jobs(HelloWorldJob) # <- required in Nautobot 2.x
